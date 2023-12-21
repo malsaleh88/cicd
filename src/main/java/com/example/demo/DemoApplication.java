0d8f5b06566acbac3d8cb0d74831da5bfd03a136
malsaleh <mohammad.alsaleh@uclouvain.be>
2023-12-21 21:03:08 +0100

    code

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+
+    @GetMapping("/welcome")
+    public String welcome(){
+     return "Welcome to ci / cd";
+    }
+
+    //echo "# cicd" >> README.md
+    //git init
+    //git add README.md
+    //git commit -m "first commit"
+    //git branch -M main
+    //git remote add origin https://github.com/malsaleh88/cicd.git
+    //git push -u origin main
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
